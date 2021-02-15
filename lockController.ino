f50256dc2571f5f1502be09d81b7ac06c825e8f6
KrissiBT <minecraft.man96@gmail.com>
2021-02-15 10:39:07 +0000

    added lock status monitor  and a reboot function
    function that monitors the status of a lock in order to notify the server when a lock status changes
    and added a reboot function (that i haven't tried ) to possibly fix an issue i found that crashes the lock after 30 operations

--- lockController.ino
+++ lockController.ino
@@ -1,5 +1,5 @@
 #include <StringSplitter.h>
-
+#include <avr/wdt.h>
 //--------------------------------------------------------------- This is the firmware for the cabinets locks 
 //---------------------------- it uses a uart link between the locks and is written with the goal that its semi infinitely scaleable by limiting the need for every lock to have its own id
 String command;
@@ -7,11 +7,15 @@ boolean command_started = false;
 boolean command_ready = false;
 char receivedChar;
 boolean newData = false;
+const int lockSens = 12;
+int lockerId = -1;//--------------- set locker id for unkown when the locker opens for the first time it will know its id
+int lockSensStatus = 3;
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(4, OUTPUT); //------------- Setup pin for unlocking locker
     Serial.begin(115200);
+    pinMode(lockSens,INPUT_PULLUP);
     //Serial.println("<Arduino is ready>");
 }
 
@@ -19,6 +23,7 @@ void loop() {
     recvOneChar();
     getCommand();
     showNewData();
+    dealWithLockStatus();
     
 }
 
@@ -28,6 +33,39 @@ void recvOneChar() {
         newData = true;
     }
 }
+
+
+void reboot() {
+  wdt_disable();
+  wdt_enable(WDTO_15MS);
+  while (1) {}
+}
+
+
+//==================================================== Function to watch over changes in lock status to notify the server if a locker opens or closes 
+void dealWithLockStatus(){
+     if (digitalRead(lockSens) != lockSensStatus){
+        delay(500);
+        lockSensStatus = digitalRead(lockSens);
+        
+        if (lockSensStatus == 0){
+          Serial.print("<");
+          Serial.print(lockerId);
+          Serial.print(":");
+          Serial.print("C");
+          Serial.print(">");
+          }
+
+          if (lockSensStatus == 1){
+          Serial.print("<");
+          Serial.print(lockerId);
+          Serial.print(":");
+          Serial.print("O");
+          Serial.print(">");
+          }
+      }
+  }
+
 //------------------------------------------------ Stiching togater the command from uart since you need to send uart data char by char
 void getCommand() {
   if (newData == true && command_started == true && receivedChar != '>') {
@@ -93,15 +131,16 @@ void getCommand() {
           //Serial.println("hey thats me");
           digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
           digitalWrite(4,HIGH);
-          delay(3000);                       // wait for a second
+          delay(1000);                       // wait for a second
           digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-          digitalWrite(4,LOW);
-          delay(3000); 
+          digitalWrite(4,LOW); 
           Serial.print("<");
           Serial.print(id);
           Serial.print(":");
           Serial.print("O");
           Serial.print(">");
+          lockerId = id;
+          
         }
 
        
